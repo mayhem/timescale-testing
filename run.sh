4cf040f4afcfc0a34fa3e8f3305a59b2c631e1ed
Robert Kaye <rob@musicbrainz.org>
2020-02-22 11:17:58 +0000

    Use a fancy pants sort

--- run.sh
+++ run.sh
@@ -4,8 +4,7 @@ echo "-- concat dump"
 ./concat_dump.py dump complete_dump.json
 
 echo "-- sort dump"
-sort complete_dump.json > complete_dump_sorted.json
-rm complete_dump.json
+sort --parallel=4 --compress-program=bzip2 complete_dump.json > complete_dump_sorted.json.bz2
 
 echo "-- import dump"
 ./import_dump.py complete_dump_sorted.json
