cac3f23141591a9d957d42b84fd1417f6a7299e8
Robert Kaye <rob@musicbrainz.org>
2020-02-22 00:35:50 +0000

    merge sort the hard way

--- import_dump.py
+++ import_dump.py
@@ -36,11 +36,13 @@ class ListenImporter(object):
                 try:
                     curs.execute(CREATE_LISTEN_TABLE_QUERY)
                     curs.execute("SELECT create_hypertable('listen', 'listened_at')")
+                    print("created table")
                     self.conn.commit()
                     break
 
                 except DuplicateTable as err:
                     self.conn.rollback()
+                    print("dropped old table")
                     curs.execute("DROP TABLE listen")
                     self.conn.commit()
 
@@ -62,76 +64,109 @@ class ListenImporter(object):
         print("Inserted %d rows in %.3f, %d rows/s. Total %d" % (len(listens), t1-t0, int(len(listens)/(t1-t0)), self.total))
 
 
-    def parse_listens_file(self, filename):
+    def insert_files(self, files):
 
-        print("import ", filename)
+        file_count = len(files)
         listens = []
-        with open(filename, "rt") as f:
-             while True:
-                line = f.readline()
-                if not line:
-                    break
+        done = False
+        while not done:
+            lowest_ts = datetime.datetime(2038, 1, 1) # lol this will break before time_t rolls over. Ha!
+            lowest_index = -1
+            last_ts = datetime.datetime(1970, 1, 1)
+            for i, f in enumerate(files):
+                if f[1][0] < lowest_ts:
+                    lowest_index = i
+                    lowest_tw = f[1][0]
 
-                data = ujson.loads(line)
-                tm = data['track_metadata']
-                if tm['artist_name']:
-                    tm['artist_name'] = tm['artist_name'].replace("\u0000", "")
-                if tm['track_name']:
-                     tm['track_name'] = tm['track_name'].replace("\u0000", "")
-                if tm['release_name']:
-                    tm['release_name'] = tm['release_name'].replace("\u0000", "")
-                listens.append([
-                    datetime.datetime.utcfromtimestamp(data['listened_at']),
-                    data['recording_msid'],
-                    data['user_name'],
-                    ujson.dumps(tm)])
+                    if f[0]:
+                        # replace the consumed listen
+                        new_listen = self.prepare_listen(f[0])
+                        if new_listen:
+                            f[1] = new_listen
+                        else:
+                            f[0] = None
+                            file_count -= 1
+                            if file_count == 0:
+                                done = True
+                                break
 
+            assert(lowest_index != -1)
+            assert(lowest_ts >= last_ts);
+            last_ts = lowest_ts
+            listens.append(f[1])
 
-    def write_chunk(self, chunk):
-        FINISH ME!
-
-        for blah blah:
             if len(listens) == 50000:
-                try:
-                    self.write_listens(listens)
-                except psycopg2.errors.UntranslatableCharacter:
-                    print(listens)
+                self.write_listens(listens)
                 listens = []
 
         self.write_listens(listens)
 
 
-    def collect_listens(self, path):
+    def prepare_listen(self, f):
 
-        for chunk in num_chunks:
-            for filename in os.listdir(path):
-                if filename in ['.', '..']:
-                    continue
+        line = f.readline()
+        data = ujson.loads(line)
+        tm = data['track_metadata']
+        if tm['artist_name']:
+            tm['artist_name'] = tm['artist_name'].replace("\u0000", "")
+        if tm['track_name']:
+             tm['track_name'] = tm['track_name'].replace("\u0000", "")
+        if tm['release_name']:
+            tm['release_name'] = tm['release_name'].replace("\u0000", "")
 
-                new_path = os.path.join(path, filename)
-                if os.path.isdir(new_path):
-                    self.find_listen_files(self.conn, new_path)
-
-                if filename.endswith(".listens"):
-                    self.parse_listens(self.conn, new_path)
-
-
-    def import_dir(self, listens_dir, num_chunks):
-
-        chunks = [ {} for i in range(num_chunks) ]
+        return (datetime.datetime.utcfromtimestamp(data['listened_at']),
+             data['recording_msid'],
+             data['user_name'],
+             ujson.dumps(tm))
 
 
+    def open_dump_files(self, path):
+
+        files = []
+        for filename in os.listdir(path):
+            if filename in ['.', '..']:
+                continue
+
+            new_path = os.path.join(path, filename)
+            if os.path.isdir(new_path):
+                files.extend(self.open_dump_files(new_path))
+
+            if filename.endswith(".listens"):
+                print("open", new_path)
+                f = open(new_path, "r")
+                files.append([f, self.prepare_listen(f)])
+
+        return files
 
 
 
 @click.command()
 @click.argument("listens_dir", nargs=1)
-@click.argument("chunks", nargs=1)
-def import_listens(listens_dir, chunks):
+def import_listens(listens_dir):
     with psycopg2.connect('dbname=listenbrainz user=listenbrainz host=localhost password=listenbrainz') as conn:
         li = ListenImporter(conn)
         li.create_tables()
-        li.import_dir(listens_dir, chunks)
+        try:
+            files = li.open_dump_files(listens_dir)
+        except IOError as err:
+            print(err)
+            return
+        except OSError as err:
+            print(err)
+            return
+
+        try:
+            li.insert_files(files);
+        except IOError as err:
+            print(err)
+            return
+        except OSError as err:
+            print(err)
+            return
+        except psycopg2.OperationalError as err:
+            print(err)
+            return
+
 
 
 def usage(command):
