68a2d4d36278970ae779c13a2e1d2966b20629fc
Robert Kaye <rob@musicbrainz.org>
2020-05-02 19:32:42 +0000

    Read compressed dump file and write output straight to disk.

--- concat_dump.py
+++ concat_dump.py
@@ -2,45 +2,38 @@
 
 import sys
 import os
+import tarfile
 import ujson
 
 
-def output_dump_file(outf, dump_file):
+def output_dump_file(outf, src_file):
 
-    print(dump_file)
-    with open(dump_file, "r") as src_file:
-        while True:
-            line = src_file.readline()
-            if not line:
-                break
+    while True:
+        line = src_file.readline()
+        if not line:
+            break
 
-            data = ujson.loads(line)
-            out = ("%012d-" % data['listened_at']) + line
-            outf.write(out)
+        line = line.decode("utf-8")
+        data = ujson.loads(line)
 
-
-def concat_dump(f, dump_dir, output_file):
-
-    opened = False
-    if not f:
-        f = open(output_file, "w")
-        opened = False
-
-    for filename in os.listdir(dump_dir):
-        if filename in ['.', '..']:
+        # skip listens with a 0 timestamp
+        if data['listened_at'] == 0:
             continue
 
-        new_path = os.path.join(dump_dir, filename)
-        if os.path.isdir(new_path):
-            concat_dump(f, new_path, output_file)
+        out = ("%012d-" % data['listened_at']) + line
+        outf.write(out)
 
-        if filename.endswith(".listens"):
-            output_dump_file(f, new_path)
 
-    if opened:
-        f.close()
+def concat_dump(dump_file, output_file):
+
+    with open(output_file, 'w') as outfile:
+        with tarfile.open(dump_file, 'r:xz') as tar:
+            for tarinfo in tar:
+                if tarinfo.isreg() and tarinfo.name.endswith(".listens"):
+                    print(tarinfo.name)
+                    output_dump_file(outfile, tar.extractfile(tarinfo))
 
 
 if __name__ == "__main__":
-    concat_dump(None, sys.argv[1], sys.argv[2])
+    concat_dump(sys.argv[1], sys.argv[2])
     sys.exit(0)
